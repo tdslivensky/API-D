8e1d0155d7b9c1a3112e3a4a2310258ef02e87b4
Tim Slivensky <tslivensky@emailatg.com>
2020-03-16 10:36:51 -0500

    finalize API

--- API/courseAPI/models.py
+++ API/courseAPI/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+	name = models.CharField(max_length = 250)
+	language = models.CharField(max_length = 100)
+	price = models.DecimalField(max_digits = 21, decimal_places = 2)
+	
+	def __str__(self):
+		return self.name
